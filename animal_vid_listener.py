1e66187102e94207ffafdaa101b55d4e6e8f8e0c
Alexander Koumis <me@alexander.computer>
2017-04-23 17:15:52 -0700

    Initial commit

---
+++ animal_vid_listener.py
@@ -0,0 +1,44 @@
+import traceback
+
+from tweepy.streaming import StreamListener
+
+
+class AnimalVidListener(StreamListener):
+
+
+    def __init__(self, api, *args, **kwargs):
+        super(StreamListener, self).__init__(*args, **kwargs)
+        self.api = api
+
+    def on_status(self, status):
+        try:
+            if hasattr(status, 'retweeted_status'):
+                return
+            if self._adult_post(status):
+                return
+            video_link = self._status_video_link(status)
+            if video_link is not None:
+                self.api.retweet(status.id)
+        except Exception as exc:
+            # Trying to figure out what kind of exception this throws
+            print('Exception[{}] in on_status: {}, text: {}'.format(type(exc), str(exc), status.text))
+            traceback.print_stack()
+
+    def on_error(self, status_code):
+        print('Error: {}'.format(status_code))
+        if status_code == 420:
+            # returning False in on_data disconnects the stream
+            return False
+
+    def _adult_post(self, status):
+        for word in ['sex', 'sexy', 'porn', 'fuck', 'nude', 'playboy', 'screwed', 'hot']:
+            if word in status.text:
+                return True
+        return False
+
+    def _status_video_link(self, status):
+        if 'media' in status.entities:
+            for item in status.entities['media']:
+                if 'video' in item['expanded_url']:
+                    return item['expanded_url']
+        return None
---
+++ app.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+
+import tweepy
+
+from animal_vid_listener import AnimalVidListener
+
+
+def get_args():
+    """Parse user args, get config file path"""
+    parser = argparse.ArgumentParser(description='SendBeatsBot app')
+    parser.add_argument('config_filename', help='Path to JSON config file.')
+    return parser.parse_args()
+
+def load_config(config_filename):
+    """Read the config file and parse the JSON"""
+    with open(config_filename, 'r') as config_file:
+        return json.load(config_file)
+
+def run(config):
+    """Run the main logic"""
+    auth = tweepy.OAuthHandler(config['consumer_key'], config['consumer_secret'])
+    auth.set_access_token(config['access_token'], config['access_token_secret'])
+
+    api = tweepy.API(auth)
+
+    listener = AnimalVidListener(api)
+
+    stream = tweepy.Stream(auth, listener)
+    stream.filter(track=['animal,dog,cat,lion,cheetah,zoo,bird,leopard,boar,deer,giraffe,chicken,'
+                         'mouse,dachshund,buffalo,donkey,horse,monkey,bear,pug,pig,rabbit,sheep,'
+                         'llama,elephant,hyena,alligator,turtle'])
+
+def main():
+    """Main app outer loop"""
+    args = get_args()
+    config = load_config(args.config_filename)
+    # Keep the app running when it periodically hangs
+    while True:
+        try:
+            run(config)
+        except Exception as exc:
+            # Trying to figure out what kind of exception this throws
+            print('Exception type in main(): {}, exception: {}'.format(type(exc), str(exc)))
+
+if __name__ == '__main__':
+    main()
